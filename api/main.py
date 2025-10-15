9cdbf4d7c2505d6902a14dd81046188c11f1902b
DeshanSamarathunga2001 <deshansamarathunga13@gmail.com>
2025-10-15 10:45:18 +0530

    Add image prediction endpoint with model loading
    Introduces TensorFlow model loading and a /predict endpoint to accept image uploads for prediction. Adds image reading utility and updates server port to 8001.

--- api/main.py
+++ api/main.py
@@ -1,19 +1,34 @@
+from PIL.Image import Image
 from fastapi import FastAPI, File, UploadFile
 import uvicorn
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
+
 app = FastAPI()
+
+MODEL = tf.keras.models.load_model("../models/1.0.keras")
+CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
+
 
 @app.get("/ping")
 async def ping():
     return "Hello, I am alive"
 
 
+def read_file_as_image(data) -> np.ndarray:
+    image =np.array.Image.open(BytesIO(data))
+
 @app.post("/predict")
 async def predict(
         file: UploadFile = File(...)
 ):
-    pass
+    image = read_file_as_image(await file.read())
+
+    return
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="localhost",port=8000)
+    uvicorn.run(app, host="localhost",port=8001)
 
